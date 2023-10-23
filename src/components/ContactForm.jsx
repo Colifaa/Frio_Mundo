3cacfae809ca61f3184ca352e11e837492b66a95
santiperez98 <santip19982022@gmail.com>
2023-10-23 00:59:46 -0300

    stylossobrenosotros

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, FormControl, FormLabel, Input, Textarea, Text } from '@chakra-ui/react';
 
 const ContactForm = () => {
@@ -6,11 +6,11 @@ const ContactForm = () => {
     backgroundImage: "url('footerice.png')",
     backgroundSize: 'cover',
     backgroundPosition: 'center',
-    minHeight: '100vh', // Establece la altura mínima de la pantalla completa
+    minHeight: '100vh',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: '16px', // Agrega espacio interno para mejorar la apariencia en pantallas más pequeñas
+    padding: '16px',
   };
 
   const formContainerStyle = {
@@ -18,7 +18,7 @@ const ContactForm = () => {
     padding: '20px',
     borderRadius: '8px',
     maxWidth: '500px',
-    width: '100%', // Asegura que el formulario ocupe todo el ancho de la pantalla
+    width: '100%',
   };
 
   const titleStyle = {
@@ -33,6 +33,28 @@ const ContactForm = () => {
     color: 'white',
   };
 
+  const [formData, setFormData] = useState({
+    nombre: '',
+    email: '',
+    telefono: '',
+    mensaje: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleFormSubmit = () => {
+    const mensajeWhatsApp = `Nombre: ${formData.nombre}%0AEmail: ${formData.email}%0ATeléfono: ${formData.telefono}%0AMensaje: ${formData.mensaje}`;
+    const numeroWhatsApp = '+5492604609798'; // Reemplaza con el número de WhatsApp al que deseas enviar el mensaje
+    const whatsappURL = `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`;
+    window.open(whatsappURL, '_blank');
+  };
+
   return (
     <Box style={containerStyle}>
       <Box style={formContainerStyle}>
@@ -40,25 +62,52 @@ const ContactForm = () => {
         <form>
           <FormControl isRequired>
             <FormLabel color="white">Nombre</FormLabel>
-            <Input type="text" placeholder="Su nombre" style={inputStyle} />
+            <Input
+              type="text"
+              name="nombre"
+              placeholder="Su nombre"
+              style={inputStyle}
+              value={formData.nombre}
+              onChange={handleInputChange}
+            />
           </FormControl>
 
           <FormControl isRequired mt="4">
             <FormLabel color="white">Email</FormLabel>
-            <Input type="email" placeholder="Su email" style={inputStyle} />
+            <Input
+              type="email"
+              name="email"
+              placeholder="Su email"
+              style={inputStyle}
+              value={formData.email}
+              onChange={handleInputChange}
+            />
           </FormControl>
 
           <FormControl isRequired mt="4">
             <FormLabel color="white">Número de teléfono</FormLabel>
-            <Input type="tel" placeholder="Su número de teléfono" style={inputStyle} />
+            <Input
+              type="tel"
+              name="telefono"
+              placeholder="Su número de teléfono"
+              style={inputStyle}
+              value={formData.telefono}
+              onChange={handleInputChange}
+            />
           </FormControl>
 
           <FormControl isRequired mt="4">
-            <FormLabel color="white">Mensaje con alguna de las opciones de arriba</FormLabel>
-            <Textarea placeholder="Su mensaje" style={inputStyle} />
+            <FormLabel color="white">Mensaje</FormLabel>
+            <Textarea
+              name="mensaje"
+              placeholder="Su mensaje"
+              style={inputStyle}
+              value={formData.mensaje}
+              onChange={handleInputChange}
+            />
           </FormControl>
 
-          <Button type="submit" mt="4" colorScheme="teal">
+          <Button type="button" mt="4" colorScheme="teal" onClick={handleFormSubmit}>
             Enviar
           </Button>
         </form>
